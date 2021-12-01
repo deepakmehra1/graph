a4be28a5c1dd1277f7077d1e1f9b0337c8e4f11e
Deepak Mehra <92205452+deepakmehra1@users.noreply.github.com>
2021-12-01 03:58:36 -0800

    Update adjacency_list.cpp

--- adjacency_list.cpp
+++ adjacency_list.cpp
@@ -1,1 +1,41 @@
 
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+//v is no of vertices
+#define v 5
+
+void addedge(vector<int>graph[],int x,int y)
+{
+    graph[x].push_back(y);
+}
+
+int main()
+{
+    vector<int> graph[v];
+    
+    addedge(graph,0,1);
+    addedge(graph,0,4);
+    addedge(graph,1,1);
+    addedge(graph,1,2);
+    addedge(graph,1,3);
+    addedge(graph,1,4);
+    addedge(graph,3,2);
+    addedge(graph,3,4);
+
+    cout<<"adjacency list : "<<endl;
+    for (int i = 0; i < v; i++)
+    {
+        cout<<i;
+
+        for (int j = 0; j < graph[i].size(); j++)
+        {
+            cout<<"->"<<graph[i][j]<<" ";
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
